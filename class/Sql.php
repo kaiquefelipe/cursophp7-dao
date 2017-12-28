00b3594d3844f46e202cfbf20c1d3769b7c4782c
Kaique Felipe <kaique.felipe.67@gmail.com>
2017-12-27 22:51:01 -0300

    Criando a classe Usuário

--- class/Sql.php
+++ class/Sql.php
@@ -14,13 +14,13 @@ class Sql extends PDO{
 		// $con = new PDO("sqlsrv:Server=DELL;Database=dbphp7;ConnectionPooling=1", $usuarioServer, $senhaServer);
 	}
 
-	private function setParam($statment, $key, $value){
-		$statment->bindParam($key, $value);
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
 	}
 
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 		foreach ($parameters as $key => $value) {
-			$this->setParam($key, $value);
+			$this->setParam($statement, $key, $value);
 		}
 	}
 
--- class/Usuario.php
+++ class/Usuario.php
@@ -1,7 +1,67 @@
 <?php  
 
-class Usuario extends Sql{
-	
+class Usuario {
+	private $idusuario;
+	private $deslogin;
+	private $dessenha;
+	private $dtcadastro;
+
+	public function getIdUsuario(){
+		return $this->idusuario;
+	}
+
+	public function setIdUsuario($valor){
+		$this->idusuario = $valor;
+	}
+
+	public function getDesLogin(){
+		return $this->deslogin;
+	}
+
+	public function setDesLogin($valor){
+		$this->deslogin = $valor;
+	}
+
+	public function getDesSenha(){
+		return $this->dessenha;
+	}
+
+	public function setDesSenha($valor){
+		$this->dessenha = $valor;
+	}
+
+	public function getDtCadastro(){
+		return $this->dtcadastro;
+	}
+
+	public function setDtCadastro($valor){
+		$this->dtcadastro = $valor;
+	}
+
+	public function loadById($id){
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+			":ID"=>$id
+		));
+
+		if(count($results) > 0){
+			$row = $results[0];
+			$this->setIdUsuario($row['idusuario']);
+			$this->setDesLogin($row['deslogin']);
+			$this->setDesSenha($row['dessenha']);
+			$this->setDtCadastro(new DateTime($row['dtcadastro']));
+		}
+	}
+
+	public function __toString(){
+		return json_encode(array(
+			"Id Usuario"=>$this->getIdUsuario(),
+			"Login"=>$this->getDesLogin(),
+			"Senha"=>$this->getDesSenha(),
+			"Data de cadastro"=>$this->getDtCadastro()->format("d/m/Y H:i:s")
+		));
+	}
+
 }
 
 ?>
